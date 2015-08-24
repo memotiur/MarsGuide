a8ea3fbd2090f02eefd06a778c76c4d9c6f01dfb
SerhejShilingov <s.shilingov@gmail.com>
2015-08-24 16:55:32 +0300

    Initial commit

---
+++ app/src/main/java/com/example/admin/marsguide/Main7Activity.java
@@ -0,0 +1,47 @@
+package com.example.admin.marsguide;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class Main7Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main7);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main7, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    public void sendMessage1(View view) {
+        Intent intent = new Intent(Main7Activity.this, Main6Activity.class);
+        startActivity(intent);
+    }
+    public void sendMessage2(View view) {
+        Intent intent = new Intent(Main7Activity.this, Main8Activity.class);
+        startActivity(intent);
+    }
+}
